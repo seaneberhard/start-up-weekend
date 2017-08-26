67c99f8cacb460c91a8a5f283a9966b20d09dc0d
seaneberhard <seaneberhard@gmail.com>
2017-08-26 19:07:12 +0100

    adding code for reading/writing poptimes to disk

--- pillproject1/app/src/main/java/com/lucyeberhard/pillproject1/MainActivity.java
+++ pillproject1/app/src/main/java/com/lucyeberhard/pillproject1/MainActivity.java
@@ -1,21 +1,36 @@
 package com.lucyeberhard.pillproject1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import java.security.Timestamp;
-import java.security.cert.CertPath;
+import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String filename = "pilldata";
     public static final String PILL_RESULT = "com.example.myfirstapp.RESULT";
+
+    private Date[] poptimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        poptimes = new Date[4];
+        try {
+            Scanner in = new Scanner(openFileInput(filename));
+            for (int i = 0; i < 4; i++) poptimes[i] = new Date(in.nextLong());
+            in.close();
+        } catch (FileNotFoundException e) {
+            for (int i = 0; i < 4; i++) poptimes[i] = new Date(0);
+        }
+    }
+
+    private long timeLeft() {
+        return 0;
     }
 
     /**
