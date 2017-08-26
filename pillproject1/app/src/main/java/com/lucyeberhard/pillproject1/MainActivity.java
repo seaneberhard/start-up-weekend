43b2af9cec7afb4dc7238c9d642cc96dc401691e
seaneberhard <seaneberhard@gmail.com>
2017-08-26 23:27:52 +0100

    Finished the basic back end

--- pillproject1/app/src/main/java/com/lucyeberhard/pillproject1/MainActivity.java
+++ pillproject1/app/src/main/java/com/lucyeberhard/pillproject1/MainActivity.java
@@ -1,52 +1,68 @@
 package com.lucyeberhard.pillproject1;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
-import java.io.FileNotFoundException;
 import java.util.Date;
-import java.util.Scanner;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String filename = "pilldata";
-    public static final String PILL_RESULT = "com.example.myfirstapp.RESULT";
-
-    private Date[] poptimes;
+    private static final String TIME_FORMAT = "%02d:%02d:%02d";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        poptimes = new Date[4];
-        try {
-            Scanner in = new Scanner(openFileInput(filename));
-            for (int i = 0; i < 4; i++) poptimes[i] = new Date(in.nextLong());
-            in.close();
-        } catch (FileNotFoundException e) {
-            for (int i = 0; i < 4; i++) poptimes[i] = new Date(0);
-        }
+        countdown();
     }
 
-    private long timeLeft() {
-        return 0;
-    }
-
-    /**
-     * Called when the user taps the Send button
+    /*
+    oh boy oh boy oh boy oh boy
      */
+    private void countdown() {
+        final TextView text = (TextView) findViewById(R.id.countdown);
+        new CountDownTimer(timeLeft(), 1000) {
+            @Override
+            public void onTick(long l) {
+                text.setText(String.format(Locale.US, TIME_FORMAT, l / (3600 * 1000), l / (60 * 1000) % 60, l / 1000 % 60));
+            }
 
-    public void getTime(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        // Do something in response
+            @Override
+            public void onFinish() {
+                text.setText("Pop Pill");
+            }
+        }.start();
+    }
 
-        Date myDate = new Date();
+    /*
+      Time till party time.
+     */
+    private long timeLeft() {
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+        long now = new Date().getTime();
+        long d1 = now - pref.getLong("-1", 0) - 4*3600*1000;
+        long d2 = now - pref.getLong("-4", 0) - 24*3600*1000;
+        return Math.max(0, Math.max(d1, d2));
+    }
 
-        String result = myDate.toString();
-
-        intent.putExtra(PILL_RESULT, result);
-        startActivity(intent);
+    /*
+     Party time!
+     Called when user taps pop
+     */
+    public void pop(View view) {
+        if (timeLeft() > 0) return;
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putLong("-4", pref.getLong("-3", 0));
+        editor.putLong("-3", pref.getLong("-2", 0));
+        editor.putLong("-2", pref.getLong("-1", 0));
+        editor.putLong("-1", new Date().getTime());
+        editor.apply();
+        countdown();
     }
 }
 
