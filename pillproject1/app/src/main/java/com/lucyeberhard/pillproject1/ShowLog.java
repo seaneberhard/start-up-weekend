e34c9f03e8f4be557a8fbe94b4ae93423d7ddb9e
seaneberhard <seaneberhard@gmail.com>
2017-08-28 08:58:05 +0100

    Trying to add log/about functionality. attempt 2

---
+++ pillproject1/app/src/main/java/com/lucyeberhard/pillproject1/ShowLog.java
@@ -0,0 +1,15 @@
+package com.lucyeberhard.pillproject1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ShowLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_log);
+        ((TextView) findViewById(R.id.TextView01)).setText(getPreferences(MODE_PRIVATE).getString("log",""));
+    }
+}
